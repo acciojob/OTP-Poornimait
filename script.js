22d0310f9cc8aa5ea5020ebec24db0746bb2db10
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-03-02 21:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 //your JS code here. If 
 function focusNext(currentInput) {
-            const maxLength = parseInt(currentInput.getAttribute('maxlength'));
-            const nextInput = currentInput.nextElementSibling;
+	pass
+}
 
-            if (currentInput.value.length === maxLength && nextInput) {
-                nextInput.focus();
-            }
-        }
 
-        function focusPrev(currentInput) {
-            const prevInput = currentInput.previousElementSibling;
 
-            if (currentInput.value === '' && prevInput) {
-                prevInput.focus();
-            }
-        }
+function focusPrev(currentInput) {
+	pass
+}
